34f600cae48b18cc6cbfbf87994902b1ef90bdd4
Stephenrobs2003 <121466110+Stephenrobs2003@users.noreply.github.com>
2025-01-21 19:45:48 -0600

    Update assignment06.py

--- assignment06.py
+++ assignment06.py
@@ -1,5 +1,6 @@
 import random
 
+# Function to clean the plaintext by removing spaces, converting to lowercase, and padding with random letters if necessary
 def cleanPlaintext(plaintext, key):
     plaintext1 = plaintext.replace(" ", "")
     plaintext1 = plaintext1.lower()
@@ -12,10 +13,9 @@ def cleanPlaintext(plaintext, key):
         for i in range(reminder):
             random_letter = random.choice(alphabet)
             plaintext1 = plaintext1 + random_letter
-
     return plaintext1
 
-
+# Function to determine the column order based on the key
 def columnOrder(key):
     if len(key) >= 10:
         return "error"
@@ -28,58 +28,40 @@ def columnOrder(key):
             column_number = column_number + 1
         return key
 
-def printColumnarCipher(columnOrder,plaintext):
+# Function to print the columnar cipher table
+def printColumnarCipher(columnOrder, plaintext):
     text = columnOrder + plaintext
     for i in range(len(text)):
         print(text[i], end='')
         if 0 == (i + 1) % 6 and i != 0:
             print("\n", end='')
 
-
+# Function to get the text of a specific column in the plaintext
 def getColumnText(plaintext, key, columnIndex):
     text = ""
-    for i in range(columnIndex,len(plaintext),len(key)):
-
+    for i in range(columnIndex, len(plaintext), len(key)):
         text = text + plaintext[i]
     return text
 
-
+# Function to encode the plaintext using the columnar cipher method
 def columnarCipherEncode(printTable):
     plaintext = input("Enter the plaintext: ")
     key = input("Enter the key: ")
     if len(key) > 10:
         print("error")
         return
-    text = cleanPlaintext(plaintext,key)
+    text = cleanPlaintext(plaintext, key)
     variable = columnOrder(key)
-    if printTable == True:
-        printColumnarCipher(variable,text)
+    if printTable:
+        printColumnarCipher(variable, text)
     if len(key) == 1:
         getColumnText(text, key, 0)
-    # odd_number = 0
     start_index = 1
     line = ""
     for i in range(len(text)):
         if start_index > len(key):
             start_index = 0
-
-    #     start_index = start_index + columnOrder(key).find(int(column_number))
-        line = line + getColumnText(text,key,start_index)
+        line = line + getColumnText(text, key, start_index)
         start_index = start_index + 2
-        # if odd_number > 1:
-        #     start_index = start_index + 1
-        # else:
-        #     column_number = column_number + 1
-
     print(line)
     return
-
-
-
-
-
-
-
-
-
-
